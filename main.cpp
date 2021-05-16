685d7c6e6fe60c8362d06f36b9db01618344d14c
St00gan <stefanroche@protonmail.com>
2021-05-16 02:07:14 +0100

    Basic Message Printer

--- main.cpp
+++ main.cpp
@@ -1,6 +1,16 @@
-#include <iostream>
+#include "iostream"
+
+#include "sleepy_discord/sleepy_discord.h"
+
+class Client : public SleepyDiscord::DiscordClient {
+public:
+    using SleepyDiscord::DiscordClient::DiscordClient;
+    void onMessage(SleepyDiscord::Message message) override {
+        std::cout << message.author.username + ": " + message.content << std::endl;
+    }
+};
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+    Client client("token", SleepyDiscord::USER_CONTROLED_THREADS);
+    client.run();
 }
