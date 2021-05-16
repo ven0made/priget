44bc3c9f9a39a91af400903d9a1002cdbd7d8c6d
St00gan <stefanroche@protonmail.com>
2021-05-16 06:21:50 +0100

    Load config and set token

--- main.cpp
+++ main.cpp
@@ -1,16 +1,26 @@
-#include "iostream"
+//I would region this off but fuck pragma region, cringe shit, https://github.com/microsoft/vscode/issues/22289
 
 #include "sleepy_discord/sleepy_discord.h"
+
+#include "config.h"
+
+using namespace std;
+
+string token = "token";
 
 class Client : public SleepyDiscord::DiscordClient {
 public:
     using SleepyDiscord::DiscordClient::DiscordClient;
     void onMessage(SleepyDiscord::Message message) override {
-        std::cout << message.author.username + ": " + message.content << std::endl;
+        cout << message.author.username + ": " + message.content << endl;
     }
 };
 
 int main() {
-    Client client("token", SleepyDiscord::USER_CONTROLED_THREADS);
+
+    LoadConfig();
+
+    cout << token << endl;
+    Client client(token, SleepyDiscord::USER_CONTROLED_THREADS);
     client.run();
 }
