04ddd36c2a7bbdec3bb8a059b13623de7a79001e
St00gan <stefanroche@protonmail.com>
2021-05-16 06:19:58 +0100

    Add config file "priget.cfg" to load token from file

---
+++ config.cpp
@@ -0,0 +1,25 @@
+#include "config.h"
+#include "split.h"
+
+void LoadConfig() {
+    //Why the fuck are libconfig's docs so shit that doing it this way seems easier
+    //If you see this, try and implement it, I'm not wasting my time: http://hyperrealm.com/libconfig/libconfig_manual.html
+
+    fstream config;
+
+    config.open("priget.cfg",ios::in);
+    if (config.is_open()){
+        string foo;
+        while(getline(config, foo)){
+            if(foo.rfind('#',0) != 0) {
+                vector<string> x = split(foo,'=');
+                //TODO write a switch on string function here
+                if (x[0] == "token" && x.size() == 2 && !x[1].empty()){
+                    token = x[1];
+                } else {
+                    cout << "Config file formatting error" << endl;
+                }
+            }
+        }
+    }
+}
---
+++ config.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+extern string token;
+
+void LoadConfig();
